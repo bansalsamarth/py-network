08425449fe3608fb34b272e4087e115682e6730f
samarth <bansalsamarth@yahoo.com>
2014-10-05 14:06:20 +0530

    Updated

--- SocketServerCode/TCPServSS.py
+++ SocketServerCode/TCPServSS.py
@@ -5,8 +5,13 @@ HOST = ''
 PORT = 8001
 ADDR = (HOST, PORT)
 
+#The StreamRequestHandler class treats input and output
+#sockets as file-like objects
+
+
 class MyRequestHandler(SRH):
 	def handle(self):
+		#Handle method in the base class (BaseRequest) is stubbed out
 		print '...connection established with:', self.client_address
 		#Timestamp server - Prints out the time
 		self.wfile.write('[%s] %s' %(ctime(), self.rfile.readline()))
